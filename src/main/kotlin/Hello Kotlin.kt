7e8d307a6c6808580a734379c89454611efeb270
Your Name <you@example.com>
2020-08-07 12:18:11 +0800

    update pertama

---
+++ src/main/kotlin/Hello Kotlin.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val nama = "Frezza Bintang K"
+        print("Nama saya ")
+        println(nama)
+    val Hobby = "Edit Video"
+        print("Hobby saya ")
+        println(Hobby)
+}
